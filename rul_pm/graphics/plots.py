fb315cf39f6196b3c18fe322ce02dd1fa04130b9
Luciano Lorenti <lucianolorenti@gmail.com>
2020-10-20 13:16:34 +0200

    Update old name

--- rul_pm/graphics/plots.py
+++ rul_pm/graphics/plots.py
@@ -1,7 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-from rul_gcd.iterators.iterators import LifeDatasetIterator
-from rul_gcd.dataset.lives_dataset import AbstractLivesDataset
+from rul_pm.iterators.iterators import LifeDatasetIterator
+from rul_pm.dataset.lives_dataset import AbstractLivesDataset
 
 
 def plot_lives(ds : AbstractLivesDataset):
