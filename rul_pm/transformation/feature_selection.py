a127b17baa57f5643018fc43d2d6ae7eea86bc40
Luciano Lorenti <lucianolorenti@gmail.com>
2020-10-21 14:52:50 +0200

    [Transformation] Add a feature selector that discards a set of features by name

--- rul_pm/transformation/feature_selection.py
+++ rul_pm/transformation/feature_selection.py
@@ -8,7 +8,7 @@ class NullProportionSelector(BaseEstimator, TransformerMixin):
     def __init__(self, min_null_proportion=0.5):
         self.min_null_proportion = min_null_proportion
 
-    def fit(self, X):        
+    def fit(self, X, y=None):        
         self.not_null_proportion = np.mean(np.isfinite(X), axis=0)
         self.mask = self.not_null_proportion > self.min_null_proportion
         return self
@@ -21,7 +21,7 @@ class ByNameFeatureSelector(BaseEstimator, TransformerMixin):
         self.features = features
         self.features_indices = None
 
-    def fit(self, df):
+    def fit(self, df, y=None):
         if len(self.features) > 0:
             features = list(set(df.columns).intersection(set(self.features)))
         else:
@@ -36,3 +36,20 @@ class ByNameFeatureSelector(BaseEstimator, TransformerMixin):
     def n_features(self):
         return len(self.features)
 
+
+class DiscardByNameFeatureSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, features=[]):
+        self.features = features
+        self.features_indices = None
+
+    def fit(self, df, y=None):
+        features = list(set(df.columns).difference(set(self.features)))
+        self.features_indices = [i for i, c in enumerate(df.columns) if c in features]
+        return self
+
+    def transform(self, X):
+        return X.iloc[:, self.features_indices]
+
+    @property
+    def n_features(self):
+        return len(self.features)
