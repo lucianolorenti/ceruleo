3260a4a0bbd2c194d91a8574605fcfc4787fd42c
Luciano Lorenti <lucianolorenti@gmail.com>
2020-10-23 20:02:30 +0200

    Add two methods for compute picewise RUL
    * By Quantile
    * Using a constant threshold

--- rul_pm/transformation/target.py
+++ rul_pm/transformation/target.py
@@ -2,13 +2,26 @@ from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
 
 class PicewiseRUL(BaseEstimator, TransformerMixin):
-    def __init__(self, method='median'):
-        self.method = method
-        
+    def transform(self, X):
+        return np.clip(X, 0, self.max_life_)
 
     def fit(self, X, y=None):
-        self.max_life = np.quantile(X, 0.75)
         return self
 
-    def transform(self, X):
-        return np.clip(x, 0, self.max_life)
+
+class PicewiseRULQuantile(PicewiseRUL):
+    def __init__(self, quantile):
+        self.quantile = quantile
+
+    def fit(self, X, y=None):
+        self.max_life_ = np.quantile(X, self.quantile)
+        return self
+
+
+class PicewiseRULThreshold(PicewiseRUL):
+    def __init__(self, max_life):
+        self.max_life_ = max_life
+
+    def fit(self, X, y=None):
+        self.max_life_ = np.quantile(X, self.quantile)
+        return self
