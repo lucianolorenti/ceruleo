220c33fcd4682280f1787b8ba2d0a8cbf53fce6d
Luciano Lorenti <lucianolorenti@gmail.com>
2020-12-01 09:55:11 +0100

    [Transformation] Minor refactoring in feature selection module

--- rul_pm/transformation/feature_selection.py
+++ rul_pm/transformation/feature_selection.py
@@ -1,8 +1,8 @@
-import numpy as np
-from sklearn.base import TransformerMixin, BaseEstimator
-import pandas as pd
-import logging 
+import logging
 
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
 
 logger = logging.getLogger(__name__)
 
@@ -11,17 +11,19 @@ class NullProportionSelector(BaseEstimator, TransformerMixin):
     def __init__(self, min_null_proportion=0.5):
         self.min_null_proportion = min_null_proportion
 
-    def fit(self, X, y=None):        
+    def fit(self, X, y=None):
         logger.info(f'Features before NullProportionSelector {X.shape[1]}')
         self.not_null_proportion = np.mean(np.isfinite(X), axis=0)
         self.mask = self.not_null_proportion > self.min_null_proportion
-        
-        logger.info(f'Features before NullProportionSelector {np.sum(self.mask)}')
+
+        logger.info(
+            f'Features before NullProportionSelector {np.sum(self.mask)}')
         return self
 
     def transform(self, X):
-        
+
         return X[:, self.mask]
+
 
 class ByNameFeatureSelector(BaseEstimator, TransformerMixin):
     def __init__(self, features=[]):
@@ -33,15 +35,17 @@ class ByNameFeatureSelector(BaseEstimator, TransformerMixin):
             features = [f for f in self.features if f in set(df.columns)]
         else:
             features = list(set(df.columns))
-        self.features_indices = [i for i, c in enumerate(df.columns) if c in features]
+        self.features_indices = [
+            i for i, c in enumerate(df.columns) if c in features]
         return self
 
-    def transform(self, X):          
+    def transform(self, X):
         return X.iloc[:, self.features_indices]
 
     @property
     def n_features(self):
         return len(self.features)
+
 
 class LocateFeatures(BaseEstimator, TransformerMixin):
     def __init__(self, features):
@@ -50,9 +54,9 @@ class LocateFeatures(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X):                  
+    def transform(self, X):
         cols = list(X.columns)
-        for name, pos in self.features.items():            
+        for name, pos in self.features.items():
             a, b = cols.index(name), pos
             cols[b], cols[a] = cols[a], cols[b]
             X = X[cols]
@@ -66,7 +70,8 @@ class DiscardByNameFeatureSelector(BaseEstimator, TransformerMixin):
 
     def fit(self, df, y=None):
         features = list(set(df.columns).difference(set(self.features)))
-        self.features_indices = [i for i, c in enumerate(df.columns) if c in features]
+        self.features_indices = [
+            i for i, c in enumerate(df.columns) if c in features]
         return self
 
     def transform(self, X):
