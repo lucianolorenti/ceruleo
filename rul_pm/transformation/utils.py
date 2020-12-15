a7905f77d37b04b3d96054babb9a0f573adbf63b
Luciano Lorenti <lucianolorenti@gmail.com>
2020-12-15 22:07:02 +0100

    [Transformation] Add better pandas feature union

--- rul_pm/transformation/utils.py
+++ rul_pm/transformation/utils.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+import pandas as pd
 
+from sklearn.pipeline import FeatureUnion, _fit_transform_one, _transform_one
+from scipy import sparse
 
 class PandasToNumpy(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
@@ -52,47 +56,50 @@ class PandasTransformerWrapper(BaseEstimator, TransformerMixin):
         return pd.DataFrame(self.transformer.transform(X), columns=X.columns)
 
 
-import numpy as np
-import pandas as pd
-from joblib import Parallel, delayed
-from sklearn.pipeline import FeatureUnion, _fit_transform_one, _transform_one
-from scipy import sparse
+
 
 
 class PandasFeatureUnion(FeatureUnion):
     def fit_transform(self, X, y=None, **fit_params):
         self._validate_transformers()
-        result = Parallel(n_jobs=self.n_jobs)(
-            delayed(_fit_transform_one)(
+        result = [
+            _fit_transform_one(
                 transformer=trans,
                 X=X,
                 y=y,
                 weight=weight,
                 **fit_params)
-            for name, trans, weight in self._iter())
-
+            for name, trans, weight in self._iter()]
+        
         if not result:
             # All transformers are None
             return np.zeros((X.shape[0], 0))
         Xs, transformers = zip(*result)
         self._update_transformer_list(transformers)
+        
         if any(sparse.issparse(f) for f in Xs):
             Xs = sparse.hstack(Xs).tocsr()
         else:
             Xs = self.merge_dataframes_by_column(Xs)
         return Xs
 
-    def merge_dataframes_by_column(self, Xs):
-        return pd.concat(Xs, axis="columns", copy=False)
+    def merge_dataframes_by_column(self, Xs):  
+        X = pd.concat(Xs, axis="columns", copy=False, ignore_index=True)
+        names = []
+        for Xs, (name, trans, weight) in zip(Xs, self._iter()):
+            for n in Xs.columns:
+                names.append(f'{name}_{n}')
+        X.columns = names
+        
+        return X
 
     def transform(self, X):
-        Xs = Parallel(n_jobs=self.n_jobs)(
-            delayed(_transform_one)(
+        Xs = [_transform_one(
                 transformer=trans,
                 X=X,
                 y=None,
                 weight=weight)
-            for name, trans, weight in self._iter())
+            for name, trans, weight in self._iter()]        
         if not Xs:
             # All transformers are None
             return np.zeros((X.shape[0], 0))
