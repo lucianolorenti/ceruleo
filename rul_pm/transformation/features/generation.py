3bdbfde379f856d26fa0da3eac0b705a07dd8450
Luciano Lorenti <lucianolorenti@gmail.com>
2020-12-28 11:14:11 +0100

    [Transformation] Update feature generation classes

--- rul_pm/transformation/features/generation.py
+++ rul_pm/transformation/features/generation.py
@@ -1,15 +1,17 @@
 import copy
 import logging
+from typing import Optional
 
 import numpy as np
 import pandas as pd
+import scipy
 from rul_pm.transformation.utils import PandasToNumpy, TargetIdentity
+from scipy.signal import find_peaks, firwin, istft, lfilter, stft
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import FeatureUnion, Pipeline
 from sklearn.preprocessing import OneHotEncoder, RobustScaler
 from sklearn.utils.validation import check_is_fitted
-from scipy.signal import  istft, stft, find_peaks
 
 logger = logging.getLogger(__name__)
 
@@ -38,31 +40,9 @@ class LifeCumSum(BaseEstimator, TransformerMixin):
                             .cumsum())
             for c in self.columns:
                 X_new.loc[X[self.life_id_col] == life,
-                      f'{c}_cumsum'] = data_columns[c]
+                          f'{c}_cumsum'] = data_columns[c]
         return X_new
 
-
-class Diff(BaseEstimator, TransformerMixin):
-    def __init__(self, ignored=[]):        
-        self.ignored = ignored
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        
-        X_ignored = None
-        columns = X.columns
-        if len(self.ignored) > 0:
-            X_ignored = X[self.ignored] 
-            columns = [f for f in X.columns if f not in self.ignored]
-        X_new = X[columns].diff().fillna(0)
-        X_new.columns = [f'{f}_diff' for f in columns]
-        if X_ignored is not None:
-            columns = X_new.columns.values.tolist() + X_ignored.columns.values.tolist()
-            X_new =  pd.concat((X_new, X_ignored), axis='columns', ignore_index=True)
-            X_new.columns = columns
-        return X_new
 
 class LifeExceededCumSum(BaseEstimator, TransformerMixin):
     def __init__(self, columns=None,  life_id_col='life', lambda_=0.5):
@@ -83,10 +63,9 @@ class LifeExceededCumSum(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        
-        
+
         cnames = [f'{c}_cumsum' for c in self.columns]
-        new_X = pd.DataFrame({}, columns=cnames, index=X.index)            
+        new_X = pd.DataFrame({}, columns=cnames, index=X.index)
         for life in X[self.life_id_col].unique():
             data = X[X[self.life_id_col] == life]
             data_columns = data[self.columns]
@@ -104,169 +83,225 @@ class LifeExceededCumSum(BaseEstimator, TransformerMixin):
 
 
 class OneHotCategoricalPandas(BaseEstimator, TransformerMixin):
-    def __init__(self, features):
-        self.features = features
+    def __init__(self, feature):
+        self.feature = feature
+        self.encoder = None
 
     def fit(self, X, y=None):
-        self.columns = [c for c in X.columns if c in self.features]
-        self.enconder = OneHotEncoder(
-            handle_unknown='ignore', sparse=False).fit(X[self.columns])
-        logger.info(f'Categorical featuers {self.columns}')
+
+        self.encoder = (
+            OneHotEncoder(handle_unknown='ignore', sparse=False)
+            .fit(X[self.feature].values.reshape(-1, 1)))
         return self
 
     def transform(self, X, y=None):
-        return self.enconder.transform(X[self.columns])
-
-
-class LowPassFilter(BaseEstimator, TransformerMixin):
-    def __init__(self, freq):
-        self.freq = freq
-
-    def fit(self):
-        return self
-
-    def transform(self, X, y):
-        series = TimeSeries(train_dataset[ds][fff[idx]].fillna(0).values)
-
-        ax.plot(series.lowpass(self.freq))
-
-
-class RollingMean(BaseEstimator, TransformerMixin):
-    def __init__(self, window):
-        self.window = window
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        return X.rolling(self.window).mean().fillna(0)
-
-
-class RollingStd(BaseEstimator, TransformerMixin):
-
-    def __init__(self, window):
-        self.window = window
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        return X.rolling(self.window).std().fillna(0)
-
-
-class RollingRootMeanSquare(BaseEstimator, TransformerMixin):
-
-    def __init__(self, window):
-        self.window = window
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        return (X.pow(2)
-                .rolling(self.window)
-                .mean()
-                .pow(1./2)                
-                .fillna(0))
-
-
-class RollingSquareMeanRootedAbsoluteAmplitude(BaseEstimator, TransformerMixin):
-    """
-    Remaining Useful Life Prediction Based on aBi-directional LSTM Neural Network
-    """
-
-    def __init__(self, window):
-        self.window = window
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        return (X.abs()
-                .pow(1./2)
-                .rolling(self.window)
-                .mean()
-                .pow(2)                
-                .fillna(0))
-
-
-class RollingPeakValue(BaseEstimator, TransformerMixin):
-    """
-    Remaining Useful Life Prediction Based on aBi-directional LSTM Neural Network
-    """
-
-    def __init__(self, window):
-        self.window = window
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        def peak_value(data):
-            k = len(data)
-            return np.sum((data - data.mean())**4) / ((k - 1) * data.std()**4)
-        return (X
-                .rolling(self.window)
-                .apply(peak_value, raw=False)
-                .fillna(0))
-
-
-class Peaks(BaseEstimator, TransformerMixin):
-
-
-    def __init__(self, window):
-        self.window = window
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        cnames = [f'{c}_peaks' for c in X.columns]
-        new_X = pd.DataFrame(np.zeros((len(X.index), len(cnames)), dtype=np.float32), columns=cnames, index=X.index)
-        for c in X.columns:
-            peaks, _ = find_peaks(X[c])
-            new_X.loc[:, f'{c}_peaks'].iloc[peaks] = 1
-        return new_X
-        
-
-
-from scipy.signal import  firwin, lfilter
+        categories = [
+            f'{self.feature}_{l}' for l in self.encoder.categories_[0]]
+        d = self.encoder.transform(X[self.feature].values.reshape(-1, 1))
+        return pd.DataFrame(d,
+                            index=X.index,
+                            columns=categories)
 
 
 class LowFrequencies(BaseEstimator, TransformerMixin):
     def __init__(self, window):
-        self.window = window 
+        self.window = window
 
     def fit(self, X, y=None):
-        return self 
+        return self
 
     def _low(self, signal, t):
-        a = firwin(self.window+1, cutoff = 0.01, window="hann", pass_zero='lowpass')
+        a = firwin(self.window+1, cutoff=0.01,
+                   window="hann", pass_zero='lowpass')
         return lfilter(a, 1, signal)
 
     def transform(self, X, y=None):
         cnames = ([f'{c}_low' for c in X.columns])
-        new_X = pd.DataFrame(np.zeros((len(X.index), len(cnames)), dtype=np.float32), columns=cnames, index=X.index)    
-        for c in X.columns:            
+        new_X = pd.DataFrame(np.zeros((len(X.index), len(cnames)),
+                                      dtype=np.float32), columns=cnames, index=X.index)
+        for c in X.columns:
             new_X.loc[:, f'{c}_low'] = self._low(X[c], 0)
         return new_X
 
 
 class HighFrequencies(BaseEstimator, TransformerMixin):
     def __init__(self, window):
-        self.window = window 
+        self.window = window
 
     def fit(self, X, y=None):
         return self
 
     def _high(self, signal, t):
-        a = firwin(self.window+1, cutoff = 0.2, window="hann", pass_zero='highpass')
+        a = firwin(self.window+1, cutoff=0.2,
+                   window="hann", pass_zero='highpass')
         return lfilter(a, 1, signal)
 
     def transform(self, X, y=None):
         cnames = [f'{c}_high' for c in X.columns]
-        new_X = pd.DataFrame(np.zeros((len(X.index), len(cnames)), dtype=np.float32), columns=cnames, index=X.index)    
-        for c in X.columns:            
+        new_X = pd.DataFrame(np.zeros((len(X.index), len(cnames)),
+                                      dtype=np.float32), columns=cnames, index=X.index)
+        for c in X.columns:
             new_X.loc[:, f'{c}_high'] = self._high(X[c], 0)
         return new_X
 
+
+class MedianFilter(BaseEstimator, TransformerMixin):
+    def __init__(self, window: int, min_periods: int = 15):
+        self.window = window
+        self.min_periods = min_periods
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X.rolling(self.window, min_periods=self.min_periods).median()
+
+
+class MeanFilter(BaseEstimator, TransformerMixin):
+    def __init__(self, window: int, min_periods: int = 15):
+        self.window = window
+        self.min_periods = min_periods
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X.rolling(self.window, min_periods=self.min_periods).mean()
+
+
+def rolling_kurtosis(s: pd.Series, window, min_periods):
+    return s.rolling(window, min_periods=min_periods).kurt(skipna=True)
+
+
+class RollingStatistics(BaseEstimator, TransformerMixin):
+    def __init__(self, window, min_periods: int = 15, to_compute=None):
+        self.window = window
+        self.min_periods = min_periods
+        valid_stats = ['kurtosis', 'skeweness', 'mean', 'max',
+                       'min', 'std', 'peak', 'impulse', 'clearance',
+                       'rms', 'shape', 'crest', 'spectral_kurtosis']
+        if to_compute is None:
+            self.to_compute = ['kurtosis', 'skeweness', 'max',
+                               'min', 'std', 'peak', 'impulse', 'clearance',
+                               'rms', 'shape', 'crest', 'spectral_kurtosis']
+        else:
+            self.to_compute = to_compute
+
+    def fit(self, X, y=None):
+        return self
+
+    def _mean(self, s: pd.Series):
+        return s.rolling(self.window, min_periods=self.min_periods).mean(skipna=True)
+
+    def _kurtosis(self, s: pd.Series):
+        return s.rolling(self.window, min_periods=self.min_periods).kurt(skipna=True)
+
+    def _skeweness(self, s: pd.Series):
+        return s.rolling(self.window, min_periods=self.min_periods).skew(skipna=True)
+
+    def _max(self, s: pd.Series):
+        return s.rolling(self.window, min_periods=self.min_periods).max(skipna=True)
+
+    def _min(self, s: pd.Series):
+        return s.rolling(self.window, min_periods=self.min_periods).min(skipna=True)
+
+    def _std(self, s: pd.Series):
+        return s.rolling(self.window, min_periods=self.min_periods).std(skipna=True)
+
+    def _peak(self, s: pd.Series):
+        return (s.rolling(self.window, min_periods=self.min_periods).max(skipna=True) -
+                s.rolling(self.window, min_periods=self.min_periods).min(skipna=True))
+
+    def _spectral_kurtosis(self, s: pd.Series):
+        def spectral_kurtosis(x):
+            return scipy.stats.kurtosis(np.abs(np.fft.rfft(x)))
+        return s.rolling(self.window).apply(spectral_kurtosis)
+
+    def _impulse(self, s: pd.Series):
+        return self._peak(s) / s.abs().rolling(self.window, min_periods=self.min_periods).mean()
+
+    def _clearance(self, s: pd.Series):
+        return self._peak(s) / s.abs().pow(1./2).rolling(self.window, min_periods=self.min_periods).mean().pow(2)
+
+    def _rms(self, s: pd.Series):
+        return (s.pow(2)
+                 .rolling(self.window, min_periods=self.min_periods)
+                 .mean(skipna=True)
+                 .pow(1/2.))
+
+    def _shape(self, s: pd.Series):
+        return self._rms(s) / s.abs().rolling(self.window, min_periods=self.min_periods).mean(skipna=True)
+
+    def _crest(self, s: pd.Series):
+        return self._peak(s) / self._rms(s)
+
+    def transform(self, X):
+
+        X_new = pd.DataFrame(index=X.index)
+
+        for c in X.columns:
+            for stats in self.to_compute:
+                X_new[f'{c}_{stats}'] = getattr(self, f'_{stats}')(X[c])
+
+        return X_new
+
+
+class ExpandingStatistics(BaseEstimator, TransformerMixin):
+    def __init__(self, min_points=2,  to_compute=None):
+        self.min_points = min_points
+        valid_stats = ['kurtosis', 'skeweness', 'max',
+                       'min', 'std', 'peak', 'impulse', 'clearance',
+                       'rms', 'shape', 'crest']
+        if to_compute is None:
+            self.to_compute = ['kurtosis', 'skeweness', 'max',
+                               'min', 'std', 'peak', 'impulse', 'clearance',
+                               'rms', 'shape', 'crest']
+        else:
+            self.to_coExpandingStatistics
+
+    def _kurtosis(self, s: pd.Series):
+        return s.expanding(self.min_points).kurt(skipna=True)
+
+    def _skeweness(self, s: pd.Series):
+        return s.expanding(self.min_points).skew(skipna=True)
+
+    def _max(self, s: pd.Series):
+        return s.expanding(self.min_points).max(skipna=True)
+
+    def _min(self, s: pd.Series):
+        return s.expanding(self.min_points).min(skipna=True)
+
+    def _std(self, s: pd.Series):
+        return s.expanding(self.min_points).std(skipna=True)
+
+    def _peak(self, s: pd.Series):
+        return (s.expanding(self.min_points).max(skipna=True) -
+                s.expanding(self.min_points).min(skipna=True))
+
+    def _impulse(self, s: pd.Series):
+        return self._peak(s) / s.abs().expanding(self.min_points).mean()
+
+    def _clearance(self, s: pd.Series):
+        return self._peak(s) / s.abs().pow(1./2).expanding(self.min_points).mean().pow(2)
+
+    def _rms(self, s: pd.Series):
+        return (s.pow(2)
+                 .expanding(self.min_points)
+                 .mean(skipna=True)
+                 .pow(1/2.))
+
+    def _shape(self, s: pd.Series):
+        return self._rms(s) / s.abs().expanding(self.min_points).mean(skipna=True)
+
+    def _crest(self, s: pd.Series):
+        return self._peak(s) / self._rms(s)
+
+    def transform(self, X):
+
+        X_new = pd.DataFrame(index=X.index)
+
+        for c in X.columns:
+            for stats in self.to_compute:
+                X_new[f'{c}_{stats}'] = getattr(self, f'_{stats}')(X[c])
+
+        return X_new
