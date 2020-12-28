0b2083e52e5ec0d27ef72f7f0cadb8a69614c225
Luciano Lorenti <lucianolorenti@gmail.com>
2020-12-28 11:06:20 +0100

    [Iterators] Fix bug in the signal generator

--- rul_pm/iterators/utils.py
+++ rul_pm/iterators/utils.py
@@ -23,16 +23,21 @@ def windowed_signal_generator(signal_X, signal_y, i: int, window_size: int, outp
     initial = max(i - window_size+1, 0)
     signal_X_1 = signal_X[initial:i+1, :]
     if len(signal_y.shape) == 1:
+
         signal_y_1 = signal_y[i:min(i+output_size, signal_y.shape[0])]
+
         if signal_y_1.shape[0] < output_size:
             padding = np.zeros(output_size - signal_y_1.shape[0])
             signal_y_1 = np.hstack((signal_y_1, padding))
+        signal_y_1 = np.expand_dims(signal_y_1, axis=1)
     else:
         signal_y_1 = signal_y[i:min(i+output_size, signal_y.shape[0]), :]
+
         if signal_y_1.shape[0] < output_size:
-            padding = signal_y_1[-1, :].tolist() * \
-                (output_size - signal_y_1.shape[0])
-            signal_y_1 = np.hstack((signal_y_1, padding))
+            padding = np.zeros(
+                ((output_size - signal_y_1.shape[0]), signal_y_1.shape[1]))
+            signal_y_1 = np.concatenate((signal_y_1, padding), axis=0)
+
     if signal_X_1.shape[0] < window_size:
 
         signal_X_1 = np.vstack((
@@ -40,4 +45,5 @@ def windowed_signal_generator(signal_X, signal_y, i: int, window_size: int, outp
                 window_size - signal_X_1.shape[0],
                 signal_X_1.shape[1])),
             signal_X_1))
+
     return (signal_X_1, signal_y_1)
