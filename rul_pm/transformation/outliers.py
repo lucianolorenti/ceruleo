6405c0c3c9913c5e4a3bb51a09e5c21e30c47f1d
Luciano Lorenti <lucianolorenti@gmail.com>
2020-12-28 11:11:59 +0100

    [Transformation] Update IQROutlierRemover

--- rul_pm/transformation/outliers.py
+++ rul_pm/transformation/outliers.py
@@ -1,7 +1,9 @@
 import numpy as np
-from sklearn.base import TransformerMixin, BaseEstimator
-from sklearn.utils.validation import check_is_fitted
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import StandardScaler
+from sklearn.utils.validation import check_is_fitted
+
 
 class IQROutlierRemover(BaseEstimator, TransformerMixin):
     """
@@ -10,27 +12,37 @@ class IQROutlierRemover(BaseEstimator, TransformerMixin):
     imput.fit(X)
     X_t = imput.transform(X)
     """
-    def __init__(self, l=1.5, proportion_to_sample=0.1):
+
+    def __init__(self, l=1.5, proportion_to_sample=1.0):
         self.l = l
         self.proportion_to_sammple = proportion_to_sample
 
     def fit(self, X):
-        X = X[np.random.choice(X.shape[0], int(X.shape[0]*self.proportion_to_sammple), replace=False), :]
-        Q = np.quantile(X, [0.25, 0.75], axis=0)
-        self.Q1 = Q[0, :]
-        self.Q3 = Q[1, :]        
-        self.IQR = self.Q3 - self.Q1
+        if self.proportion_to_sammple < 1:
+            sampled_points = np.random.choice(X.shape[0],
+                                              int(X.shape[0] *
+                                                  self.proportion_to_sammple),
+                                              replace=False)
+            X = X.iloc[sampled_points, :]
+
+        self.Q1 = X.quantile(0.25)
+        self.Q3 = X.quantile(0.75)
+        self.IQR = (self.Q3 - self.Q1).to_dict()
+        self.Q1 = self.Q1.to_dict()
+        self.Q3 = self.Q3.to_dict()
         return self
 
     def transform(self, X):
+        X = X.copy()
         check_is_fitted(self, 'Q1')
         check_is_fitted(self, 'Q3')
         check_is_fitted(self, 'IQR')
-        mask = (
-            (X < (self.Q1 - self.l * self.IQR)) |
-            (X > (self.Q3 + self.l * self.IQR))
-        )
-        X[mask] = np.nan
+        for c in X.columns:
+            mask = (
+                (X[c] < (self.Q1[c] - self.l * self.IQR[c])) |
+                (X[c] > (self.Q3[c] + self.l * self.IQR[c]))
+            )
+            X.loc[mask, c] = np.nan
         return X
 
 
@@ -41,19 +53,19 @@ class ZScoreOutlierRemover(BaseEstimator, TransformerMixin):
     imput.fit(X)
     X_t = imput.transform(X)
     """
+
     def __init__(self, number_of_std_allowed):
         self.number_of_std_allowed = number_of_std_allowed
         self.scaler = StandardScaler()
 
     def fit(self, X):
-        self.scaler.fit(X)        
+        self.scaler.fit(X)
         return self
 
     def transform(self, X):
         X = self.scaler.transform(X)
         X[np.abs(X) > self.number_of_std_allowed] = np.nan
-        return X
-
+        return pd.DataFrame(X, columns=X.columns, index=X.index)
 
 
 class EWMAOutlierRemover(BaseEstimator, TransformerMixin):
@@ -62,7 +74,7 @@ class EWMAOutlierRemover(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         mean = np.nanmean(X, axis=0)
-        s = np.sqrt(self.lambda_ /(2-self.lambda_))*np.nanstd(X, axis=0)    
+        s = np.sqrt(self.lambda_ / (2-self.lambda_))*np.nanstd(X, axis=0)
         self.UCL = mean + 3*s
         self.LCL = mean - 3*s
         return self
@@ -73,4 +85,4 @@ class EWMAOutlierRemover(BaseEstimator, TransformerMixin):
             (X > (self.UCL))
         )
         X[mask] = np.nan
-        return X
+        return pd.DataFrame(X, columns=X.columns, index=X.index)
