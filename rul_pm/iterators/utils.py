08a45bd9549de3eb4625f9912280ffa5a51fb141
Luciano Lorenti <lucianolorenti@gmail.com>
2021-01-04 09:41:41 +0100

    [Iterators] Move functions to utils

--- rul_pm/iterators/utils.py
+++ rul_pm/iterators/utils.py
@@ -1,49 +1,35 @@
 import numpy as np
 from rul_pm.dataset.lives_dataset import AbstractLivesDataset
+from rul_pm.transformation.transformers import (PandasToNumpy, Transformer,
+                                                simple_pipeline)
+from tqdm.auto import tqdm
 
 
-def windowed_signal_generator(signal_X, signal_y, i: int, window_size: int, output_size: int = 1):
-    """
-    Return a lookback window and the value to predict.
+def dataset_map(fun, dataset, step, transformer, window):
+    from rul_pm.iterators.batcher import get_batcher
+    batcher = get_batcher(dataset,
+                          window,
+                          512,
+                          transformer,
+                          step,
+                          shuffle=False,
+                          restart_at_end=False)
+    for X, y, w in batcher:
+        fun(X, y)
 
-    Parameters
-    ----------
-    signal_X:
-             Matrix of size (life_length, n_features) with the information of the life
-    signal_y:
-             Target feature of size (life_length)
-    i: int
-       Position of the value to predict
-    window_size: int
-                 Size of the lookback window
-    Returns
-    -------
-    tuple (np.array, float)
-    """
-    initial = max(i - window_size+1, 0)
-    signal_X_1 = signal_X[initial:i+1, :]
-    if len(signal_y.shape) == 1:
 
-        signal_y_1 = signal_y[i:min(i+output_size, signal_y.shape[0])]
+def get_features(dataset,  features, step=1, window=1):
+    t = simple_pipeline(features)
+    data = {f: [] for f in features}
 
-        if signal_y_1.shape[0] < output_size:
-            padding = np.zeros(output_size - signal_y_1.shape[0])
-            signal_y_1 = np.hstack((signal_y_1, padding))
-        signal_y_1 = np.expand_dims(signal_y_1, axis=1)
-    else:
-        signal_y_1 = signal_y[i:min(i+output_size, signal_y.shape[0]), :]
-
-        if signal_y_1.shape[0] < output_size:
-            padding = np.zeros(
-                ((output_size - signal_y_1.shape[0]), signal_y_1.shape[1]))
-            signal_y_1 = np.concatenate((signal_y_1, padding), axis=0)
-
-    if signal_X_1.shape[0] < window_size:
-
-        signal_X_1 = np.vstack((
-            np.zeros((
-                window_size - signal_X_1.shape[0],
-                signal_X_1.shape[1])),
-            signal_X_1))
-
-    return (signal_X_1, signal_y_1)
+    def populate_data(X, y):
+        for i, f in enumerate(features):
+            data[f].extend(np.squeeze(X[:, i]).tolist())
+    t = Transformer(
+        features,
+        t,
+        transformerY=PandasToNumpy()
+    )
+    t.fit(dataset)
+    dataset_map(populate_data, dataset, step, t, window)
+    return data
