128ac36588bfb380a5d7a46c8ffce25a1f79fc55
Luciano Lorenti <lucianolorenti@gmail.com>
2021-01-04 09:43:44 +0100

    [transformation.augmentation] Add RandomAugmentantor class
    This class add gaussian noise to the patterns

--- rul_pm/transformation/augmentation.py
+++ rul_pm/transformation/augmentation.py
@@ -1,19 +1,18 @@
-from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
 
 class RandomAugmentantor(BaseEstimator, TransformerMixin):
     def __init__(self, scale, augment_proability=0.2):
-        self.scale = scale 
+        self.scale = scale
         self.augment_proability = augment_proability
 
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X):
-        if np.random.rand() < self.augment_proability:
-            return X + np.random.normal(0, scale=self.scale)
-        else:
-            return X
-        
-
-
+    def transform(self, X: pd.DataFrame):
+        t = pd.DataFrame(np.random.normal(0, scale=self.scale, size=X.shape),
+                         index=X.index,
+                         columns=X.columns)
+        return X + t
