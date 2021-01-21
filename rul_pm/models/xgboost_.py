4a2d8a8c8b98c95404c1abd71eec88668e79bb86
Luciano Lorenti <lucianolorenti@gmail.com>
2021-01-22 00:14:30 +0100

    [Models] Update xgboost model

--- rul_pm/models/xgboost_.py
+++ rul_pm/models/xgboost_.py
@@ -1,9 +1,21 @@
 from rul_pm.iterators.iterators import WindowedDatasetIterator
+from rul_pm.models.model import TrainableModel
 from xgboost import XGBRegressor
 
 
-class XGBoostModel:
-    def __init__(self, window: int, step: int,  transformer, **kwargs):
+class XGBoostModel(TrainableModel):
+    def __init__(self,
+                 window: int = 15,
+                 step: int = 1,
+                 transformer=None,
+                 cache_size: int = 15,
+                 sample_weight: str = 'equal',
+                 **kwargs):
+        super().__init__(window,
+                         step,
+                         transformer,
+                         cache_size=cache_size,
+                         sample_weight=sample_weight)
         self.window = window
         self.transformer = transformer
         self.step = step
@@ -20,13 +32,13 @@ class XGBoostModel:
         return X, y
 
     def fit(self, train_dataset):
-
-        X, y = self._transform(train_dataset, fit=True)
-        self.xgbr.fit(X, y)
+        X, y, sample_weight = self.get_data(train_dataset)
+        self.xgbr.fit(X, y, sample_weight=sample_weight)
         return self
 
     def predict(self, dataset):
-        return self.xgbr.predict(self._transform(dataset)[0])
+        X, _, _ = self.get_data(dataset, shuffle=False)
+        return self.xgbr.predict(X)
 
     def feature_importances(self):
         return self.xgbr.feature_importances_.reshape((self.window, self.n_features))
