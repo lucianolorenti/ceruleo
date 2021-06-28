5e69983e5714227e8e8d3c30493c50ba0beb05d3
Luciano Lorenti <lucianolorenti@gmail.com>
2021-06-28 15:03:41 +0200

    Dataset: Obtain more information on the variance of each feature of the dataset

--- rul_pm/dataset/analysis.py
+++ rul_pm/dataset/analysis.py
@@ -1,4 +1,5 @@
 import logging
+from typing import List, Optional
 
 import numpy as np
 import pandas as pd
@@ -105,7 +106,7 @@ def null_proportion_per_life(dataset):
     return df
 
 
-def variance_information(dataset):
+def variance_information(dataset, features:Optional[List[str]]=None, transformer= None):
     """
     Return mean and max null proportion for each column of each life of the dataset
 
@@ -122,13 +123,21 @@ def variance_information(dataset):
           The key is the column name and the value is the list of null proportion
           for each life
     """
-    comon_features = [set(life.columns.tolist()) for life in dataset]
-    comon_features = comon_features[0].intersection(*comon_features)
+    common_features = []
+    for life in dataset:
+        if transformer is not None:
+            life = transformer.transform(life)
+        common_features.append(set(life.columns.tolist()))
+    common_features = common_features[0].intersection(*common_features)
+    if features:
+        common_features = set(common_features).intersection(set(features))
 
     std_per_life = {}
     for life in dataset:
+        if transformer is not None:
+            life = transformer.transform(life)
         d = life.std().to_dict()
-        for column in comon_features:
+        for column in common_features:
             if column not in d:
                 continue
             if not isinstance(d[column], float):
@@ -137,12 +146,12 @@ def variance_information(dataset):
             std_list.append(d[column])
 
     data = [
-        (column, np.min(std_per_life[column]), np.mean(std_per_life[column]))
+        (column, np.min(std_per_life[column]), np.mean(std_per_life[column]), np.max(std_per_life[column]))
         for column in std_per_life.keys()
     ]
 
     df = pd.DataFrame(
-        data, columns=["Feature", "Min std Proportion", "Mean std Proportion"]
+        data, columns=["Feature", "Min std Proportion", "Mean std Proportion", "Max std Proportion"]
     )
     df.sort_values(by="Min std Proportion", inplace=True, ascending=True)
     return df, std_per_life
