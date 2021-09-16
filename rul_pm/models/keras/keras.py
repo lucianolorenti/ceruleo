b4a3fce94d43f57d8a1f4d5688fc3ea2178c823e
Luciano Lorenti <lucianolorenti@gmail.com>
2021-09-16 12:41:58 +0200

    Models: Remove keras iterators because now they are in temporis

--- rul_pm/models/keras/keras.py
+++ rul_pm/models/keras/keras.py
@@ -11,7 +11,8 @@ from temporis.iterators.batcher import Batcher
 from rul_pm.models.model import TrainableModel
 from tensorflow.keras import backend as K
 from tensorflow.keras import optimizers
-from tensorflow.keras.callbacks import Callback, EarlyStopping, ModelCheckpoint
+
+from temporis.models.keras import keras_regression_batcher, keras_autoencoder_batcher
 
 logger = logging.getLogger(__name__)
 
@@ -20,50 +21,6 @@ def root_mean_squared_error(y_true, y_pred):
     return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
 
 
-def keras_regression_batcher(batcher):
-    n_features = batcher.n_features
-
-    def generator_function():
-        for X, y, w in batcher:
-            yield X, y, w
-
-    a = tf.data.Dataset.from_generator(
-        generator_function,
-        output_signature=(
-            tf.TensorSpec(
-                shape=(None, batcher.window_size, n_features), dtype=tf.float32
-            ),
-            tf.TensorSpec(shape=(None, batcher.output_shape, 1), dtype=tf.float32),
-            tf.TensorSpec(shape=(None, 1), dtype=tf.float32),
-        ),
-    )
-
-    if batcher.prefetch_size is not None:
-        a = a.prefetch(batcher.batch_size * 2)
-    return a
-
-
-def keras_autoencoder_batcher(batcher):
-    n_features = batcher.n_features
-
-    def gen_train():
-        for X, w in batcher:
-            yield X, X, w
-
-    a = tf.data.Dataset.from_generator(
-        gen_train,
-        (tf.float32, tf.float32, tf.float32),
-        (
-            tf.TensorShape([None, batcher.window_size, n_features]),
-            tf.TensorShape([None, batcher.window_size, n_features]),
-            tf.TensorShape([None, 1]),
-        ),
-    )
-
-    if batcher.prefetch_size is not None:
-        a = a.prefetch(batcher.batch_size * 2)
-
-    return a
 
 
 class KerasTrainableModel(TrainableModel):
