6f354078969efb6d286921f92f0bba39f2f6885f
Luciano Lorenti <lucianolorenti@gmail.com>
2021-11-26 14:06:52 +0100

    RUL_PM: Get the proper environment variables of the folders

--- rul_pm/__init__.py
+++ rul_pm/__init__.py
@@ -4,10 +4,10 @@ import os
 PACKAGE_PATH = Path(__file__).resolve().parent
 DATA_PATH = PACKAGE_PATH /'dataset' / 'data'
 
-DATASET_PATH = Path(os.environ.get('RUL_PM_DATASET_PATH',  Path.home() / '.rul_pm' / 'datasets'))
+DATASET_PATH = Path(os.getenv('RUL_PM_DATASET_PATH',  Path.home() / '.rul_pm' / 'datasets'))
 DATASET_PATH.mkdir(parents=True, exist_ok=True)
 
-CACHE_PATH = Path(os.environ.get('RUL_PM_CACHE_PATH', Path.home() / '.rul_pm' / 'cache'))
+CACHE_PATH = Path(os.getenv('RUL_PM_CACHE_PATH', Path.home() / '.rul_pm' / 'cache'))
 CACHE_PATH.mkdir(parents=True, exist_ok=True)
 
 __version__ = 0.5
