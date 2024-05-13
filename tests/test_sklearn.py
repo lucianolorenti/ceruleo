1672dea4331560c7bb56bc13248df58dffca821e
Luciano Lorenti <lucianolorenti@gmail.com>
2024-05-13 22:29:52 +0200

    Iterators: Allow sample weights to use dataframes, series and arrays (#39)
    * Iterators: Allow sample weights to use dataframes, series and arrays
    
    * Tests: Add tests for sample weight in keras, and scikit-learn
    
    * Bump Version

--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -1,16 +1,19 @@
-from ceruleo.dataset.catalog.CMAPSS import CMAPSSDataset, sensor_indices
-from ceruleo.transformation import Transformer
-from ceruleo.transformation.functional.pipeline.pipeline import make_pipeline
-from ceruleo.transformation.features.selection import ByNameFeatureSelector
-from ceruleo.transformation.features.scalers import MinMaxScaler
-from ceruleo.models.sklearn import (
-    CeruleoRegressor,
-    TimeSeriesWindowTransformer,
-    CeruleoMetricWrapper
-)
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
+
+from ceruleo.dataset.catalog.CMAPSS import CMAPSSDataset, sensor_indices
+from ceruleo.iterators.sample_weight import RULInverseWeighted
+from ceruleo.models.sklearn import (
+    CeruleoMetricWrapper,
+    CeruleoRegressor,
+    TimeSeriesWindowTransformer,
+)
+from ceruleo.transformation import Transformer
+from ceruleo.transformation.features.scalers import MinMaxScaler
+from ceruleo.transformation.features.selection import ByNameFeatureSelector
+from ceruleo.transformation.functional.pipeline.pipeline import make_pipeline
+
 
 def test_gridsearch_cv():
     train_dataset = CMAPSSDataset(train=True, models='FD001')
@@ -48,3 +51,34 @@ def test_gridsearch_cv():
 
     grid_search.fit(train_dataset)
     assert grid_search is not None
+
+
+
+def test_sample_weights():
+    train_dataset = CMAPSSDataset(train=True, models='FD001')
+    FEATURES = [train_dataset[0].columns[i] for i in sensor_indices]
+    transformer = Transformer(
+        pipelineX=make_pipeline(
+            ByNameFeatureSelector(features=FEATURES), 
+            MinMaxScaler(range=(-1, 1))
+
+        ), 
+        pipelineY=make_pipeline(
+            ByNameFeatureSelector(features=['RUL']),  
+        )
+    )
+
+
+    regressor_gs = CeruleoRegressor(
+        TimeSeriesWindowTransformer(
+            transformer,
+            window_size=32,
+            sample_weight=RULInverseWeighted(),
+            padding=True,
+            step=1),   
+        Ridge(alpha=15))
+
+   
+
+    regressor_gs = regressor_gs.fit(train_dataset)
+    assert regressor_gs is not None
